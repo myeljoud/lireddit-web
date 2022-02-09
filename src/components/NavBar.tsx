d6650e4af9fac74518f61f92150c9516180e2541
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2022-02-09 12:12:49 +0000

    Reactoring home page components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,49 @@
-import { Box, Container, HStack, Link } from "@chakra-ui/react";
+import { Box, Container, HStack, Link, Text, Button } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ fetching, error, data }] = useMeQuery();
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+
   return (
     <Box as="nav" px={2} py={4} bg="teal.100" w="100%">
       <Container maxW="container.xl">
-        <HStack spacing={6} justifyContent="flex-end">
-          <Link>
-            <NextLink href="/login">Login</NextLink>
-          </Link>
-          <Link>
-            <NextLink href="/register">Register</NextLink>
-          </Link>
+        <HStack
+          spacing={6}
+          justifyContent="flex-end"
+          alignItems={"center"}
+          h="max-content">
+          {error && <Text color="red.500">Error ...</Text>}
+
+          {data?.me ? (
+            <>
+              <Text>
+                {data.me.username} | {data.me.email}
+              </Text>
+              <Button
+                variant={"link"}
+                color="teal.700"
+                isLoading={logoutFetching}
+                onClick={() => logout()}>
+                Logout
+              </Button>
+            </>
+          ) : fetching ? (
+            <Text>Loading ...</Text>
+          ) : (
+            <>
+              <Link>
+                <NextLink href="/login">Login</NextLink>
+              </Link>
+              <Link>
+                <NextLink href="/register">Register</NextLink>
+              </Link>
+            </>
+          )}
         </HStack>
       </Container>
     </Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,8 @@
-import {
-  Box,
-  Button,
-  Container,
-  HStack,
-  Link,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import NextLink from "next/link";
-import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
-  const [{ fetching, error, data }] = useMeQuery();
-  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-  return (
-    <div>
-      <Head>
-        <title>Home page | Lireddit Inc.</title>
-        <meta name="description" content="Lireddit Inc." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <VStack spacing={10}>
-        <Box as="nav" px={2} py={4} bg="teal.100" w="100%">
-          <Container maxW="container.xl">
-            <HStack
-              spacing={6}
-              justifyContent="flex-end"
-              alignItems={"center"}
-              h="max-content">
-              {error && <Text color="red.500">Error ...</Text>}
-
-              {data?.me ? (
-                <>
-                  <Text>
-                    {data.me.username} | {data.me.email}
-                  </Text>
-                  <Button
-                    variant={"link"}
-                    color="teal.700"
-                    isLoading={logoutFetching}
-                    onClick={() => logout()}>
-                    Logout
-                  </Button>
-                </>
-              ) : fetching ? (
-                <Text>Loading ...</Text>
-              ) : (
-                <>
-                  <Link>
-                    <NextLink href="/login">Login</NextLink>
-                  </Link>
-                  <Link>
-                    <NextLink href="/register">Register</NextLink>
-                  </Link>
-                </>
-              )}
-            </HStack>
-          </Container>
-        </Box>
-        <Container maxW="container.xl">
-          <Box as="main">Hello</Box>
-        </Container>
-      </VStack>
-    </div>
-  );
+  return <Layout pageTitle="Home page">Hello</Layout>;
 };
 
 export default Home;
