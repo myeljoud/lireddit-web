d65c4309add0e332885e61bed8635d9d2c8edb6a
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2022-02-09 13:05:13 +0000

    Displaying posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,38 @@
+import { Box, Divider, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { withUrqlClient } from "next-urql";
 import Layout from "../components/Layout";
-import { usePostsQuery } from "../generated/graphql";
+import { Post, usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+
+type stateType = Post[] | string | null;
 
 const Home: NextPage = () => {
   const [{ fetching, error, data }] = usePostsQuery();
-  let state = null;
+  let state: stateType = null;
   if (fetching) {
     state = "Loading ...";
   } else if (data?.posts) {
-    state = data.posts[0].title;
+    state = data.posts;
   } else if (error) {
     state = "Error ...";
   }
 
-  return <Layout pageTitle="Home page">{state}</Layout>;
+  return (
+    <Layout pageTitle="Home page">
+      {!Array.isArray(state)
+        ? state
+        : state.map(p => (
+            <Box key={p.id}>
+              <Text mb={2} fontWeight="bold">
+                {p.title}
+              </Text>
+              <Text>{p.body}</Text>
+              <Divider my={4} />
+            </Box>
+          ))}
+    </Layout>
+  );
 };
 
 export default withUrqlClient(createUrqlClient)(Home);
