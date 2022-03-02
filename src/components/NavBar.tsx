1f1dfe2ade2929ec2198466a5541c1972b15cd66
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2022-03-02 13:04:29 +0000

    Adding add-post link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,12 @@
-import { Box, Container, HStack, Link, Text, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  HStack,
+  Link,
+  Text,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
@@ -15,38 +23,45 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
   return (
     <Box as="nav" px={2} py={4} bg="teal.100" w="100%">
       <Container maxW="container.xl">
-        <HStack
-          spacing={6}
-          justifyContent="flex-end"
-          alignItems={"center"}
-          h="max-content">
-          {error && <Text color="red.500">Error ...</Text>}
+        <HStack alignItems="center" justifyContent="space-between">
+          <NextLink href="/" passHref>
+            <Heading as="a" display="flex" size="lg" color="teal.900">
+              Lireddit.
+            </Heading>
+          </NextLink>
+          <HStack
+            spacing={6}
+            justifyContent="flex-end"
+            alignItems={"center"}
+            h="max-content">
+            {error && <Text color="red.500">Error ...</Text>}
 
-          {data?.me ? (
-            <>
-              <Text>
-                {data.me.username} | {data.me.email}
-              </Text>
-              <Button
-                variant={"link"}
-                color="teal.700"
-                isLoading={logoutFetching}
-                onClick={() => logout()}>
-                Logout
-              </Button>
-            </>
-          ) : fetching ? (
-            <Text>Loading ...</Text>
-          ) : (
-            <>
-              <NextLink href="/login" passHref>
-                <Link>Login</Link>
-              </NextLink>
-              <NextLink href="/register" passHref>
-                <Link>Register</Link>
-              </NextLink>
-            </>
-          )}
+            {data?.me ? (
+              <>
+                <Text>
+                  {data.me.username} | {data.me.email}
+                </Text>
+                <Button
+                  variant={"link"}
+                  color="teal.700"
+                  isLoading={logoutFetching}
+                  onClick={() => logout()}>
+                  Logout
+                </Button>
+              </>
+            ) : fetching ? (
+              <Text>Loading ...</Text>
+            ) : (
+              <>
+                <NextLink href="/login" passHref>
+                  <Link>Login</Link>
+                </NextLink>
+                <NextLink href="/register" passHref>
+                  <Link>Register</Link>
+                </NextLink>
+              </>
+            )}
+          </HStack>
         </HStack>
       </Container>
     </Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import { Box, Divider, Text } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { Box, Button, Divider, Flex, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { withUrqlClient } from "next-urql";
+import NextLink from "next/link";
 import Layout from "../components/Layout";
 import { Post, usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
@@ -22,17 +24,31 @@ const Home: NextPage = () => {
 
   return (
     <Layout pageTitle="Home page">
-      {!Array.isArray(state)
-        ? state
-        : state.map(p => (
-            <Box key={p.id}>
-              <Text mb={2} fontWeight="bold">
-                {p.title}
-              </Text>
-              <Text>{p.body}</Text>
-              <Divider my={4} />
-            </Box>
-          ))}
+      <Flex justifyContent="flex-end">
+        <NextLink href="/posts/create-post" passHref>
+          <Button
+            as="a"
+            rightIcon={<AddIcon />}
+            textTransform="uppercase"
+            colorScheme="teal"
+            variant="outline">
+            Create new post
+          </Button>
+        </NextLink>
+      </Flex>
+      <Box mt={8}>
+        {!Array.isArray(state)
+          ? state
+          : state.map(p => (
+              <Box key={p.id}>
+                <Text mb={2} fontWeight="bold">
+                  {p.title}
+                </Text>
+                <Text>{p.body}</Text>
+                <Divider my={4} />
+              </Box>
+            ))}
+      </Box>
     </Layout>
   );
 };
